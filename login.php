ebe4249a9adf6ce32075dd77d9617a04a85be838
Edwin Quevedo <edwinqs27@gmail.com>
2019-11-10 15:25:33 -0500

    Avance de la vista login

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php session_start(); ?>
+<html>
+	<head>
+		<title>Inicio de sesiòn</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/banner.php"; ?>
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+		<h2>Inicio de sesiòn</h2>
+
+		
+</div>
+</div>
+</div>
+		
+	</body>
+</html>
