7e60fbcc0b288df093ccd0b093cd0722f65bfb3a
oscar <oscar86077@gmail.com>
2019-11-11 17:37:43 -0500

    Comparto encabezado banner.php

---
+++ php/banner.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default" role="navigation">
+<div class="container">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="home.php"><b> <div align="left"><img src="imagenes/logo-crm.jpg" width='100px' heigth='120px'title="Ir a la pàgina principal"> </div>
+   </b></a>
+    
+      <div align="center"><img src="imagenes/logo_head.png" width='150px' heigth='120px'> </div>
+  <br>
+    
+  </div>
+    <div align="left"><img src="imagenes/compusep-02.png" width='200px' heigth='200px'> </div> 
+ 
+  </div>
+</nav>
