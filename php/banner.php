b3dcd0b6d8746ba5fbf4b186a5325a132e1904bb
jeny <jeny-s88@hotmail.com>
2019-11-24 11:23:57 -0500

    Segundo Avance de la vista navbar.php

--- php/banner.php
+++ php/banner.php
@@ -7,14 +7,25 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="home.php"><b> <div align="left"><img src="imagenes/logo-crm.jpg" width='100px' heigth='120px'title="Ir a la pàgina principal"> </div>
-   </b></a>
+    <a class="navbar-brand" href="index.php"><b><div align="left"><img src="imagenes/logo-crm.jpg" width='100px' heigth='120px' title="Hola aca registras la opcion"> </div>
+   </b></a></b></a>
     
-      <div align="center"><img src="imagenes/logo_head.png" width='150px' heigth='120px'> </div>
-  <br>
-    
+  <div align="center"><img src="imagenes/logo_head.png" width='150px' heigth='120px' title="Hola acà ingresas a las opciones"> </div>
   </div>
-    <div align="left"><img src="imagenes/compusep-02.png" width='200px' heigth='200px'> </div> 
- 
+
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li><a class=navbar-brand href="ver.php">VER</a></li>
+    </ul>
+<form class="navbar-form navbar-left" role="search" action="./buscar.php">
+      <div class="form-group">
+        <input type="text" name="s" class="form-control" placeholder="Buscar">
+      </div>
+      <button type="submit" class="btn btn-default">&nbsp;<i class="glyphicon glyphicon-search"></i>&nbsp;</button>       
+</form>
+         <div align="left"><img src="imagenes/compusep-02.png" width='200px' heigth='200px' title="Ir a la pàgina principal"> </div>  
+        
   </div>
+     <div align="right"> <?php include "./home.php"; ?></div>
+</div>
 </nav>
