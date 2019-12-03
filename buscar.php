faba80b207e724312e0bfcb1475c7b6915b03d9f
jeny <jeny-s88@hotmail.com>
2019-12-02 22:30:15 -0500

    Avance buscar.php

---
+++ buscar.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>CRUD</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+		<script src="js/jquery.min.js"></script>
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+
+<script src="bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
