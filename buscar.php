e22603510d24e6572dedb3c58f43582a47a7b753
jeny <jeny-s88@hotmail.com>
2019-12-10 20:16:36 -0500

    Segundo Avance vista buscar.php

--- buscar.php
+++ buscar.php
@@ -6,6 +6,15 @@
 	</head>
 	<body>
 	<?php include "php/navbar.php"; ?>
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+		<h2>BUSCAR</h2>
+
+<?php include "php/busqueda.php"; ?>
+</div>
+</div>
+</div>
 
 <script src="bootstrap/js/bootstrap.min.js"></script>
 	</body>
